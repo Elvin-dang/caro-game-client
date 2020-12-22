063604cadaefc736a409c8c503f7c30cbe51835f
Vinh <vinhdang871@gmail.com>
2020-12-22 22:32:22 +0700

    Fix some bugs p2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ import Room from '../components/Room';
 import MessageList from '../components/MessageList';
 import Profile from '../components/Profile';
 function Home() {
-	const socket = io.connect(process.env.REACT_APP_test_api_domain_withoutAPI,
+	const socket = io.connect(process.env.REACT_APP_api_domain_withoutAPI,
 		{
 			transports: ['websocket'], 
 			upgrade: false
