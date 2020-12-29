9a3ba2691cb063a33822b2fde267f58f380a1859
Đặng Xuân Vinh <36046078+Elvin-dang@users.noreply.github.com>
2020-12-29 22:40:49 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ import Profile from '../components/Profile';
 import AnotherProfile from '../components/AnotherProfile';
 import TopPlayer from '../pages/TopPlayer'
 function Home() {
-	const socket = io.connect(process.env.REACT_APP_test_api_domain_withoutAPI,
+	const socket = io.connect(process.env.REACT_APP_api_domain_withoutAPI,
 		{
 			transports: ['websocket'], 
 			upgrade: false
